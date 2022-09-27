5615c73f3ccc3d9b09c6583cfdf63634f0a3b463
gustamamrf <gustamamarufimelda@gmail.com>
2022-09-27 16:35:32 +0700

    menyelesaaikan tugas chalenge react

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,19 @@
 // Kalian bisa menambahkan CSS di src/components/Header.css
 // atau langsung tambahkan dengan sx={{}}
 import React from 'react';
+import { Typography, Divider } from '@mui/material';
 
 // Header berisi judul dan subtitle
 // Judul: Call a Friend
 // Subtitle: Your friendly contact app
 const Header = () => {
   return (
-    <></>
+    <div>
+      <Typography variant="h3" component="h4" sx={{ marginTop: '10px' }}>
+        Call a Friend
+      </Typography>
+      <Divider sx={{ fontSize: '15px', marginTop: '10px' }}>Your friendly contact app</Divider>
+    </div>
   );
 };
 
